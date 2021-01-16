f074dc0e262c94dc7cf75488bc17b01de7246b20
audreyr01 <75100931+audreyr01@users.noreply.github.com>
2021-01-15 21:50:41 -0800

    Built Hourly Forecast

--- src/index.js
+++ src/index.js
@@ -120,11 +120,52 @@ function showTemperature(response) {
   weatherIconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#hourly-forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  console.log(forecast);
+
+  for (let index = 0; index < 6; index++) {
+    let forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+  <div class="col-2 forecast">
+      ${formatHours(forecast.dt * 1000)}
+      <br />
+      <img
+      src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+      />
+      <br />
+      <strong>↑${Math.round(forecast.main.temp_max)}°</strong>
+      <br />
+      ↓${Math.round(forecast.main.temp_min)}°
+  `;
+  }
+}
+
 function searchCity(city) {
   let units = "imperial";
   let apiKey = "f082d16967881db977592af7768cccb0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(`${apiUrl}`).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Convert °C
